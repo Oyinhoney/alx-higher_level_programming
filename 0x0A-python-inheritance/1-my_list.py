d9860f77fb72d10911884a5bd9e2c93f5789cc8d
Oyinhoney <shukurah4254@gmail.com>
2022-08-01 15:39:30 +0100

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""My List class Documentation"""
+
+
+class MyList(list):
+    """MyList class inherits from the python class list"""
+
+    def print_sorted(self):
+        """this function sorts the current instance of the class MyList"""
+
+        print(sorted(self))
